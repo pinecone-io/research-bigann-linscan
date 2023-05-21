460ca8091c3c7841ef1b7c1e223b532c9501a9c1
Amir Ingber <ingber@pinecone.io>
2023-05-21 20:36:38 +0300

    example in main

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ mod index;
 fn main() {
     let mut ind = index::Index::new();
     let v1 = HashMap::from([(1_u32, 0.4_f32), (5, 0.6)]);
-    let v2 = HashMap::from([(1_u32, 0.4_f32), (5, 0.9)]);
+    let v2 = HashMap::from([(2_u32, 0.4_f32), (5, 0.9)]);
     let q = HashMap::from([(13_u32, 0.4_f32), (5, 1.2)]);
     ind.insert(&v1);
     ind.insert(&v2);
+
+    ind.print_stats();
+
     let r = ind.retrieve(&q, 4, None);
     println!("{:?}", &r);
 }
