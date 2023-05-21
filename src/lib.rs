4781033edd0447b8316b5347af3f4d000dc8efaa
Amir Ingber <ingber@pinecone.io>
2023-05-21 20:33:32 +0300

    python wrapper

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use pyo3::prelude::*;
+mod index;
+
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyclass]
+struct LinscanIndex {
+    index: index::Index
+}
+
+#[pymethods]
+impl LinscanIndex {
+    #[new]
+    pub fn new() -> LinscanIndex {
+        println!("Initializing a new LinscanIndex, now with stats!");
+        LinscanIndex {
+            index: index::Index::new(),
+        }
+    }
+
+    pub fn insert(&mut self, newdoc: HashMap<u32, f32>) {
+        self.index.insert(&newdoc);
+    }
+
+    pub fn retrieve(&mut self, query: HashMap<u32, f32>, top_k: usize) -> Vec<u32> {
+        let r = self.index.retrieve(&query, top_k, None);
+        // dbg!(r);
+        r.into_iter().map(|f| f.docid).collect()
+    }
+
+    pub fn print_stats(&self) {
+        self.index.print_stats();
+    }
+}
+
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn linscan(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<LinscanIndex>()?;
+    Ok(())
+}
